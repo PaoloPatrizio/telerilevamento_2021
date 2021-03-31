cff4de18f4525b8a5bfc727a2d5d1e0cd9babc46
PaoloPatrizio <80513523+PaoloPatrizio@users.noreply.github.com>
2021-03-31 12:43:32 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -44,7 +44,7 @@ plot(p224r63_2011$B4_sre, col=clnir)
 #attraverso questa serie di comandi noi possiamo plottare insieme un certo numero di bande ognuna con una determinata coloramppalette.
 plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin")
 #la funzione plotRGB permette di proiettare insieme diverse bande, viene utilizzata anche per visuaizzare immagini con colori "reali". le bande per la visualizzazione dei colori reali devono essere montate cosi come fatto (3,2,1).
-#SCRIVERE QUI la funzione stretch
+#la funzione stretch serve a visualizzare le bande inserite da 0 a 1, anche laddove queste non coprano tutto il campo da 0 a 1 
 plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
 #in questo caso l'infrarosso vicino è montato sulla banda del rosso (e cosi viene visualizzato), il rosso su quella del verde ed il verde su quella del blu. la banda del blu viene cosi non considerata (si possono visualizzare solo 3 bande per volta).
 plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="Lin")
@@ -68,3 +68,13 @@ dev.off()
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 #attraverso lo strecth "hist" è possibile stretchare ancora di più i valori centrali, permettendo cosi una visualizzazione ancora diversa (in questo caso si evidenziano meglio differenze all'interno del patch forestale).
 #per gli studi di vegetazione l'infrarosso si usa di solito sull componente red, non c'è una visualizzazione migliore di altre. la visualizzazione migliore viene trovata empiricamente (esperienza fondamentale)
+###
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+pdf("confronto_immagini_amazzonia.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_1988,r=4,g=3,b=2,stretch="hist")
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="hist")
+plotRGB(p224r63_1988,r=4,g=3,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
+dev.off()
+#plottaggio e salvataggio in PDF delle immagini in RGB del 1988 e 2011
