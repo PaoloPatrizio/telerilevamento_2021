72da800749e521590d48e321dc408330e7276a48
PaoloPatrizio <80513523+PaoloPatrizio@users.noreply.github.com>
2021-04-12 14:29:50 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,8 +2,8 @@
 #Greenland increase of temperature
 #data and code from Emanuela Cosma
 library(raster)
+library(rasterVis)
 setwd("/Users/paolopatrizio/Desktop/lab/greenland")
-install.packages("rasterVis")
 lst_2000<-raster("lst_2000.tif")
 #all'interno del pacchetto Raster c'è una funzione chiamata "raster" che serve a creare un RasterLayer object (la funzione brick è invece utilizzata nel caso di multiple layers).
 #l'utilizzo della funzione "raster" ci permette di importare in R i file (immagini) sottostanti.
@@ -28,3 +28,25 @@ TGr
 plotRGB(TGr,1,2,3,stretch="Lin")
 #attraverso la funzione plotRGB possiamo plottare insieme (in modo sovrapposto) i file (1,2,3) prima riuniti in un unico file "RasterStack", i file corrisponderanno alle bande r,g e b, dipendentemente da come noi li inseriremo nel plotRGB.
 #la predominanza di un colore rispetto agli altri nel plotRGB indica valori più alti di lst per il file a cui abbiamo associato il determinato colore.
+cl<-colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr,col.regions=cl)
+#levelplot ci fornisce un layout maggiormente chiaro rispetto al solo plot (in questo specifico caso), permettendoci una migliore analisi multitemporale.
+#gli strati di una immagine stack si chiamano attributi (in TGr abbiamo 4 attributi: lst_20-00/05/10/15).
+levelplot(TGr,col.regions=cl,names.attr=c("July 2000","July 2005","July 2010","July 2015"))
+#è possibile modificare il layout del levelplot andando a modificare gli attributi negli argomenti della funzione levelplot. 
+#nel caso specifico del rasterstack TGr, se si vogliono modificare i titoli delle immagini (di default uguali ai nomi dei singoli file) si agisce sull'argomento names.attr che serve a nominare i singoli attributi.
+levelplot(TGr,col.regions=cl,main="LST variation in time",names.attr=c("July 2000","July 2005","July 2010","July 2015"))
+#attraverso l'argomento "main" possiamo attribuire un titolo principale al nostro levelplot
+#
+### ESERCITAZIONE MELT GROENLANDIA ###
+#
+setwd("/Users/paolopatrizio/Desktop/lab/melt")
+meltlist<-list.files(pattern="melt")
+meltimport<-lapply(meltlist,raster)
+melt<-stack(meltimport)
+levelplot(melt)
+melt_amount<-melt$X2007annual_melt - melt$X1979annual_melt
+#per vedere le differenze tra file all'interno del nostro rasterstack "melt" possiamo utilizzare una operazione di matrix algebra, ad esempio possiamo operare una differenza tra il primo ed ultimo file temporale.
+#essendo la differenza operata tra file interni ad un file più grande (il rasterstack melt) bisogna legarli a quest'ultimo con il $.
+clb<-colorRampPalette(c("blue","light blue","pink","red"))(100)
+plot(melt_amount, col=clb)
