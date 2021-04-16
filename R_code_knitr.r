00087d32997ff32c4f04f8ed2b811b359971e7a3
PaoloPatrizio <80513523+PaoloPatrizio@users.noreply.github.com>
2021-04-16 11:02:48 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -2,3 +2,5 @@
 
 setwd("/Users/paolopatrizio/Desktop/lab")
 library(knitr)
+stitch("R_code_greenland",template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+#il pacchetto knitr permette di generare report
