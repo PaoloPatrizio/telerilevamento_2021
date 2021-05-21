166f50829cefe9e3518958968b766bde4538c51c
PaoloPatrizio <80513523+PaoloPatrizio@users.noreply.github.com>
2021-05-21 10:57:11 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -1,5 +1,8 @@
 library(raster)
 library(RStoolbox)
+library(ggplot2) # serve a plottare i ggplot
+library(gridExtra) #serve a plottare i ggplot insieme
+library(viridis) #la libreria viridis serve per colorare i plot di ggplot in modo automatico
 setwd("/Users/paolopatrizio/Desktop/lab")
 #la funz. "raster" viene usata solo quando si vuole caricare una sola banda (just 1 layer) mentre la funzione "brick" ci permette di caricare immagini composte da più bande
 sent<-brick("sentinel.png")
@@ -33,3 +36,36 @@ sentpca <- rasterPCA(sent)
 #nel $model vediamo quanta variabilità è spiegata dalle componenti elaborate -> la stessa cosa più altre caratteristiche del modello possno essereviste attraverso la fnzione "summary"
 summary(sentpca$model)
 #la prima componente spiega il 67% della variabilità mentre la seconda il 32%
+plot(sentpca$map)
+#come si può vedere dal plot la PC1 spiega la maggior parte della variabilità
+sentpca
+#all'interno dell'oggetto vediamo le sue caratteristiche quali la $call, il $model, la $map e gli attributi
+pc1<-sentpca$map$PC1
+pc1sd5<-focal(pc1,w=matrix(1/25,nrow=5,ncol=5),fun=sd)
+### con la funzione "source" è possibile fornire come input uno script presente fuori da R 
+source("source_test_lezione.r")
+source("source.ggplot.r")
+#creiamo una nuova finestra vuota con la funzione "ggplot" a cui noi aggiungeremo pezzi attraverso un semplice "+"
+ggplot() + #con "ggplot()" creiamo una finestra vuota (rettangolo) dove inserire ciò che vogliamo
+#???geom_raster VEDERE LEZIONE TEMPO 1 ora e 30
+geom_raster(pc1sd5, mapping=aes(x=x,y=y,fill=layer))
+#cosi si può capire che la zona con più ata variabilità territoriale è quella in alto a sinistra (nel nostro plot), variabilità dovuta alle carattaristiche molto eterogenee della geomorfologia
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis() # con la funzione "scale_fill_viridis()" possiamo utilizzare color palette predefinite ed ottimali (anche aggiustate in funzione di disfunzioni visive)
+### esercizio con diverse color_ramp_palette predefinite e titolo  
+p1<-ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option="magma") +
+ggtitle("Standard deviation of PC1 by viridis colour scale")
+
+p2<-ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option="inferno") +
+ggtitle("Standard deviation of PC1 by viridis colour scale")
+
+p3<-ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis() +
+ggtitle("Standard deviation of PC1 by viridis colour scale")
+#attraverso la funzione "grid.arrange" possiamo plottare assieme più plot (p1,p2,p3 in questo caso)
+grid.arrange(p1,p2,p3, nrow=1)
